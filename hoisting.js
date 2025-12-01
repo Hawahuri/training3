be464f93eaf5cf70ac51df94908d95c1b55f7db5
Hawahuri <goblinkun98@gmail.com>
2025-12-01 12:04:55 +0545

    ggg

---
+++ hoisting.js
@@ -0,0 +1,31 @@
+
+//function add(x,y=5)
+//{
+    //return(x+y);
+//}
+//console.log(add(10));
+//console.log(add(10,20));
+
+//const arr1=[1,2,3,4];
+//const arr2=[6,7,8];
+// spread operator const combined=[...arr1,...arr2];
+//const combined1=[arr1,arr2];
+//console.log(combined);
+//console.log(combined1);
+
+
+//rest operator//converts single elements to array elements
+function sum(...number)
+{
+    return(number);
+}
+console.log(sum(1,2,3,4));
+
+const person={
+    name: "frijal",
+    age: 19,
+
+}
+const[name,age]=person;
+
+
